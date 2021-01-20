d4223f439ed523c436ac342f6771a8fa870897e3
Hetal <hetalchavan_3@gmail.com>
2021-01-20 17:15:27 +0530

    Add project files.

---
+++ CandyShop/Controllers/CandyController.cs
@@ -0,0 +1,31 @@
+﻿using CandyShop.Models;
+using CandyShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandyShop.Controllers
+{
+    public class CandyController:Controller
+    {
+        private readonly ICandyRepository _candyRepository;
+        private readonly ICategoryRepository _categoryRepository;
+        public CandyController(ICategoryRepository categoryRepository,ICandyRepository candyRepository) {
+            _candyRepository = candyRepository;
+            _categoryRepository = categoryRepository;
+        
+        }
+        public IActionResult List() {
+            ViewBag.CurrentCategory = "Bestsellers";
+            ViewBag.Title = "Testing";
+            //return View(_candyRepository.GetAllCandies);
+            var candyListViewModel = new CandyListViewModel();
+            candyListViewModel.Candies = _candyRepository.GetAllCandies;
+            candyListViewModel.CurrentCategory = "Best sellers";
+            return View(candyListViewModel);
+        }
+
+    }
+}
---
+++ CandyShop/Models/CategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandyShop.Models
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        public IEnumerable<Category> GetAllCategories => new List<Category>
+        { 
+            new Category{CategoryId=1,CategoryDescription="Awesome and delicious hard candy",CategoryName="Hard Candy"},
+            new Category{CategoryId=2,CategoryDescription="Scrumptious chocolate candy",CategoryName="Chocolate candy"},
+            new Category{ CategoryId=3,CategoryName="Fruit candy",CategoryDescription="Sweet and sour fruit candy"}
+            
+        };
+    }
+}

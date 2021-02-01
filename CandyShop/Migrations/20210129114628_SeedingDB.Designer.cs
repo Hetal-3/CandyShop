e32c737dd97c2ca7ba5873b60407ef2815c3acdf
Hetal <hetalchavan_3@gmail.com>
2021-02-01 16:21:54 +0530

    Moving Layout and Viewimports file

--- CandyShop/Migrations/20210129114628_SeedingDB.Designer.cs
+++ CandyShop/Migrations/20210129114628_SeedingDB.Designer.cs
@@ -3,14 +3,16 @@ using CandyShop.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CandyShop.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210129114628_SeedingDB")]
+    partial class SeedingDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
